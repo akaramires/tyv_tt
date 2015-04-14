5940539509e53a349324ea97d1f9dcfe19252690
akaramires <e.abdurayimov@gmail.com>
2015-04-14 11:31:07 +0300

    new implementation

--- src/js/app.js
+++ src/js/app.js
@@ -7,106 +7,54 @@
 
     "use strict";
 
-    var Rating = function (element, options) {
+    var Rating = function ($rating, options) {
         var defaults = {
-            background : 'transparent',
-            showCurrent: false,
-            verbose    : false
+            background: 'transparent'
         };
 
-        var $module = $(element),
-            obj = this,
+        var itemClass = 'rating_item',
+            itemActiveClass = 'rating_item_active',
 
-            settings = $.extend(true, {}, defaults, options),
-            $percentLabel = null,
-            stars_count = 0
-            ;
-
-        this.initialize = function () {
-            verbose('Initializing rating', settings);
-
-            $module.addClass('rating');
-            stars_count = $module.find('> li:not(.current)').length;
-
-            stylization();
-            assignEvents();
-
-            if (settings.showCurrent) {
-                showPercentLabel();
-            }
-        };
-
-        var hasPercentLabel = function () {
-            return ($module.find('.current span').length > 0);
-        };
-
-        var hasActiveStar = function () {
-            return ($module.find('.active').length > 0);
-        };
-
-        var createPercentLabel = function () {
-            $module
-                .prepend('<li class="current"><span></span></li>')
-            ;
-
-            $percentLabel = $module.find('li.current span');
-
-            verbose('Creating label', $percentLabel);
-        };
-
-        var stylization = function () {
-            verbose('Stylization rating', settings);
-
-            console.log($module);
-            $module
-                .css({'background': settings.background})
-            ;
-        };
-
-        var showPercentLabel = function () {
-            if (!hasPercentLabel()) {
-                createPercentLabel();
-            }
-
-            calcActivePercent();
-        };
-
-        var calcActivePercent = function () {
-            if (hasActiveStar()) {
-                calcCurrentPercent($module.find('.active').index());
-            } else {
-                calcCurrentPercent(stars_count);
-            }
-        };
-
-        var calcCurrentPercent = function (index) {
-            if ($percentLabel) {
-                var percent = (stars_count - index + 1) * (100 / stars_count);
-                $percentLabel.text(percent + '%');
-            }
-        };
+            settings = $.extend(true, {}, defaults, options);
 
         var assignEvents = function () {
-            verbose('Assigning events', settings);
-
-            $module.find('li:not(.current)').hover(function () {
-                calcCurrentPercent($(this).index());
-            });
-
-            $module.mouseleave(function () {
-                calcActivePercent();
-            });
+            $rating
+                .find('.' + itemClass)
+                .not('.rating_item_rating')
+                .hover(hoverIn, hoverOut)
+            ;
         };
 
-        var verbose = function () {
-            if (settings.verbose) {
-                console.info(arguments);
-            }
+        var hoverIn = function () {
+            $(this)
+                .addClass(itemActiveClass)
+                .prevAll()
+                .addClass(itemActiveClass);
+
+            $rating
+                .find('.rating_item_rating span')
+                .text(parseInt($(this).text()));
+        };
+
+        var hoverOut = function () {
+            $(this)
+                .removeClass(itemActiveClass)
+                .prevAll()
+                .removeClass(itemActiveClass);
+
+            $rating
+                .find('.rating_item_rating span')
+                .text(0);
+        };
+
+        this.initialize = function () {
+            $rating.css({background: settings.background});
+
+            assignEvents();
         };
     };
 
     $.fn.rating = function (options) {
-        console.log($(this.selector));
         var $rating = new Rating(this, options);
 
         $rating.initialize();
@@ -114,14 +62,8 @@
         return $rating;
     };
 
-    $('.js-rating').rating();
-
-    $('.js-rating-gray').rating({
-        background: 'lightgray'
-    });
-
-    $('.js-rating-current').rating({
-        showCurrent: true
+    $('.rating').rating({
+        background: '#ffff00'
     });
 
 })(jQuery);
